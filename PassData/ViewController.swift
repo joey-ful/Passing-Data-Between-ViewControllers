47556f2f0373e4fc2f319691d0ea832de6d1b71f
joey-ful <isjoeyful@gmail.com>
2021-07-10 07:42:17 +0900

    Initial Commit

---
+++ PassData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData
+//
+//  Created by 홍정아 on 2021/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
